43db960e4787e32ca7fd9eca323473075ef550b9
Yegon Japheth Kiptoo <jyegon504@gmail.com>
2023-06-03 16:05:50 +0300

    Completed object association methods

--- App/models/product.rb
+++ App/models/product.rb
@@ -1,4 +1,22 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(product_id: self.id, user_id: user.id, star_rating: star_rating, comment: comment)
+    end
+
+    def print_all_reviews
+        self.reviews.each do |review|
+          puts "Review for #{self.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+        end
+    end
+
+    def average_rating
+        total_reviews = self.reviews.count
+        return 0 if total_reviews.zero?
+    
+        sum_ratings = self.reviews.sum(:star_rating)
+        sum_ratings.to_f / total_reviews
+    end
 end
--- App/models/user.rb
+++ App/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    def favorite_product
+        self.products.order('reviews.star_rating DESC').first
+    end
+
+    def remove_reviews(product_to_delete)
+        self.reviews.where(product_id: product.id).destroy_all
+    end
 end
